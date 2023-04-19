5f623774cdfbdc936e7ca20699eb7c5c7f14c6dd
Jaime Daza <jaime.daza@hypersoft.co>
2023-04-19 14:02:37 -0500

    feat: :sparkles: Create and set public assets folder

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
+const path = require('path')
 const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("It works!");
-});
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
