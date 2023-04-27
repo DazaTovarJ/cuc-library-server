16ec05e1b358b60a52202a89c1a288ad8e6ab8f0
Jaime Daza <jaimedavid250701@outlook.com>
2023-04-26 23:23:16 -0500

    feat(server): :goal_net: Create 404 handler

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-const path = require('path')
+const path = require("path");
 const express = require("express");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+app.all("*", (req, res) => {
+  res.status(404).send("Page not found");
+});
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
