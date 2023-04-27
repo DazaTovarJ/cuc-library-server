ec7ef343ee2c8a1cd5b1c1c754afa45adda7a5cc
Jaime Daza <jaimedavid250701@outlook.com>
2023-04-27 09:15:27 -0500

    feat(server): :sparkles: Create form send endpoint and set form middlewares

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ const express = require("express");
 const app = express();
 
 app.use(express.static(path.join(__dirname, "..", "public")));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.post("/send", (req, res) => {
+  console.log(req.body);
+  res.send("Message received");
+});
 
 app.all("*", (req, res) => {
   res.status(404).send("Page not found");
